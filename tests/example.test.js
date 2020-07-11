33d2657bc259327ffc5478c43ecbf3cd8aad41fd
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2020-07-11 16:10:29 +0900

    test: login test added

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,13 +1,16 @@
 import HomePage from "../pages/HomePage";
+import LoginPage from "../pages/LoginPage"
 import TopBar from "../pages/components/TopBar"
 
 describe('Example', () => {
   let homepage;
+  let loginpage;
   let topbar;
 
   beforeAll(async () => {
     jest.setTimeout(15000)
     homepage = new HomePage();
+    loginpage = new LoginPage();
     topbar = new TopBar();
   })
   it('Homepage should work', async () => {
@@ -18,4 +21,11 @@ describe('Example', () => {
     await homepage.isNavbarDisplayed();
     await topbar.isTopBarDisplayed();
   });
+
+  it('try to login', async () => {
+    await loginpage.visit()
+    await loginpage.isLoginFromDisplayed();
+    await loginpage.login('username', 'password');
+    await loginpage.wait(5000);
+  })
 });
