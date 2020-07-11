179c9bdc0547c47fb36a99c8804f5fe2f94fa303
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2020-07-11 16:27:23 +0900

    test: feedback test added

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,16 +1,19 @@
 import HomePage from "../pages/HomePage";
-import LoginPage from "../pages/LoginPage"
+// import LoginPage from "../pages/LoginPage"
+import FeedbackPage from '../pages/FeedbackPage'
 import TopBar from "../pages/components/TopBar"
 
 describe('Example', () => {
   let homepage;
-  let loginpage;
+  // let loginpage;
+  let feedbackPage;
   let topbar;
 
   beforeAll(async () => {
     jest.setTimeout(15000)
     homepage = new HomePage();
-    loginpage = new LoginPage();
+    // loginpage = new LoginPage();
+    feedbackPage = new FeedbackPage();
     topbar = new TopBar();
   })
   it('Homepage should work', async () => {
@@ -22,10 +25,17 @@ describe('Example', () => {
     await topbar.isTopBarDisplayed();
   });
 
-  it('try to login', async () => {
-    await loginpage.visit()
-    await loginpage.isLoginFromDisplayed();
-    await loginpage.login('username', 'password');
-    await loginpage.wait(5000);
+  // it('try to login', async () => {
+  //   await loginpage.visit()
+  //   await loginpage.isLoginFromDisplayed();
+  //   await loginpage.login('username', 'password');
+  //   await loginpage.wait(5000);
+  // })
+
+  it('feedback should work', async () => {
+    await feedbackPage.visit();
+    await feedbackPage.isFeedbackFormDisplayed();
+    await feedbackPage.submitFeedback('hoge', 'hoge@test.com', 'subject', 'comment');
+    await feedbackPage.wait(5000);
   })
 });
